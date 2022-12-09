da0b8abfd0d405a358d34e489471a08f4636802d
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-09 00:02:23 -0300

    anotações e desenvolvimento sobre estruturas condicionais

---
+++ Primeiras Páginas Interativas com JavaScript/1. Primeiros Passos com JavaScript/2. Estruturas Condicionais (código)/aula.js
@@ -0,0 +1,11 @@
+
+const numero = 5;
+const numeroDivisivelPor5 = (numero % 5) === 0;
+
+if (numero === 0) {
+    console.log('O número é inválido');
+} else if (numero === 5) {
+    console.log('O número é inválido')
+} else {
+    console.log('Não');
+}
