b90aca3491da0a00718e82391dbf20bc2ab10a97
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-13 22:57:25 -0300

    anotações sobre API e desenvolvimento da pokedex

--- Primeiras Páginas Interativas com JavaScript/08 - pokedex/assets/js/main.js
+++ Primeiras Páginas Interativas com JavaScript/08 - pokedex/assets/js/main.js
@@ -1,9 +1,24 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset${offset}&limit=${limit}`;
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
